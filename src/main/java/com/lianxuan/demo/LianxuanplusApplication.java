fa78beaeff8bfcc9b13d3fbed238e5c348f845f5
hejianhua <hejianhua@163.com>
2020-06-08 17:45:25 +0800

    demo的第一次

---
+++ src/main/java/com/lianxuan/demo/LianxuanplusApplication.java
@@ -0,0 +1,20 @@
+package com.lianxuan.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LianxuanplusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LianxuanplusApplication.class, args);
+
+        System.out.println(
+                "wai"
+        );
+    }
+
+
+
+
+}
